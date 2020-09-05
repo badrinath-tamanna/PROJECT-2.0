abd8ca6e995e2b772595a6c15fc27543776983d1
badrinath-tamanna <badrinath.tamanna@gmail.com>
2020-09-05 22:27:11 +0530

    problem solved of hide and show password

--- HEATHY DAIRIES 2.0/All Types Of Files/Java Script/create password.js
+++ HEATHY DAIRIES 2.0/All Types Of Files/Java Script/create password.js
@@ -1,18 +1,32 @@
-function pageRedirect1()
-{
-    window.open("../Sign Up/signup.HTML",target="_self");
-}
-
-
 function pageRedirect()
 {
-    window.location.href="../Log In/login.html",target="_self";
+    window.open("../Homepage/homepage.html",target="_self");
 }
 
-function MyFunction() {
+function MyFunction1() {
     var x=document.getElementById("myInput");
     var y=document.getElementById("hide1");
     var z=document.getElementById("hide2");
+
+    if(x.type==="password"){
+        x.type="text";
+        y.style.display="block";
+        z.style.display="none";
+    }
+
+    else{
+        x.type="password";
+        y.style.display="none";
+        z.style.display="block";
+    }
+
+
+}
+
+function MyFunction2() {
+    var x=document.getElementById("myINput");
+    var y=document.getElementById("hide3");
+    var z=document.getElementById("hide4");
 
     if(x.type==="password"){
         x.type="text";
